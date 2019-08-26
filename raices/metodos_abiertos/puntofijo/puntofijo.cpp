414926d69717a92f19c35b1ea1dcc380ea22061b
Gaston Saillen <gaston_luque_29@hotmail.com>
2019-08-26 14:34:28 -0300

    Mejor aproximacion punto fijo

--- raices/metodos_abiertos/puntofijo/puntofijo.cpp
+++ raices/metodos_abiertos/puntofijo/puntofijo.cpp
@@ -3,18 +3,22 @@ using namespace std;
 #include <math.h>
 
 double fx(double x){
-    return exp(-x) - x ; // Reemplazar por cualquier funcion
+    return exp(-x) - x; // Reemplazar por cualquier funcion
 }
+ // Usar si es necesario
+//double gx(double x){
+//  return fx(x) + x;
+//}
 
-double gx(double x){
-    return fx(x) + x;
+double gp(double x){
+    return (fx(x + 0.01) - fx(x)) / 0.01; //Valor aproximado de G prima
 }
 
 int main(){
-    double xo; //x0 = x inicial
-    double xr,xrold; //valores viejos y nuevos
-    int iter; //cantidad de iteraciones
-    double es,ea; // error
+    double xo;        //x0 = x inicial
+    double xr, xrold; //valores viejos y nuevos
+    int iter;         //cantidad de iteraciones
+    double es, ea;    // error
     cout << "Ingrese la aproximación inicial: " << flush;
     cin >> xo;
     cout << "Ingrese la cantidad de iteraciones: " << flush;
@@ -24,26 +28,27 @@ int main(){
 
     xr = xo; // Asignamos el valor inicial a x nueva
 
-    for(int i = 0 ; i < iter; i++){
+    for (int i = 0; i < iter; i++){
         xrold = xr;
-        xr = gx(xrold);
+        xr = gp(xrold);
 
-        if(xr != 0){
-            ea = fabs((xr - xrold)/xr)*100;
+        if(fabs(xr) < 1){
+           ea = fabs((xr - xrold) / xr) * 100;
         }else{
+            cout << "La derivada diverge y no se puede encontrar la raiz" << endl;
             iter = i;
             break;
         }
 
-        if(ea < es){
+        if (ea < es){
             iter = i;
             break;
         }
-
     }
 
     cout << "El valor de la raiz es: " << xr << endl;
-    cout << "cantidad de iteraciones: " << iter << endl; 
+    cout << "cantidad de iteraciones: " << iter << endl;
 
     return 0;
 }
+
